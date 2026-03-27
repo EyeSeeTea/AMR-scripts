b9c6c5a1153255ef3e01c80330004e8faa1e0fb6
nshandra <34254522+nshandra@users.noreply.github.com>
2026-03-27 09:53:03 +0100

    chore: remove unused template code

--- src/domain/repositories/NotificationsRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Notification } from "domain/entities/Notification";
-import { FutureData } from "domain/entities/generic/Future";
-
-export interface NotificationsRepository {
-    send(notification: Notification): FutureData<void>;
-}
